4767f678f48783c1b38dfb62c4802be9638e87b8
Robert Cochran <robert-git@cochranmail.com>
2015-09-08 12:53:37 -0700

    Use more comprehensible variable names

--- src/quoter.cpp
+++ src/quoter.cpp
@@ -64,28 +64,28 @@ void Quoter::feed_stream(std::istream& in) {
 	/// FIRST PARSING RUN ///
 	// Read words in one by one.
 	std::string word;
-	bool sos=true, eos=false;
-	Markers eos_marker;
+	bool start_of_sentence=true, end_of_sentence=false;
+	Markers end_marker;
 	while (in >> word) {
 		// Check for end of sentence.
-		eos=true;
+		end_of_sentence=true;
 		if (word.find('.') != std::string::npos)
-			eos_marker=Markers::PERIOD;
+			end_marker=Markers::PERIOD;
 		else if (word.find('!') != std::string::npos)
-			eos_marker=Markers::EXCLAIM;
+			end_marker=Markers::EXCLAIM;
 		else if (word.find('?') != std::string::npos)
-			eos_marker=Markers::QUESTION;
+			end_marker=Markers::QUESTION;
 		else
-			eos=false;
+			end_of_sentence=false;
 
 		// Filter out unwanted characters.
 		word = filterWord(word);
 
-		if (sos) {
+		if (start_of_sentence) {
 			ParserItem_marker* newStart;
 			newStart=new ParserItem_marker(Markers::START);
 			parserItems.push_back(newStart);
-			sos=false;
+			start_of_sentence=false;
 		}
 		if (!word.empty()) {
 			ParserItem_word* newWord;
@@ -95,22 +95,22 @@ void Quoter::feed_stream(std::istream& in) {
 		// Make sure at least one word is in the current sentence
 		// if it is being ended.
 		ParserItem& last=*parserItems.back();
-		if (eos &&
+		if (end_of_sentence &&
 		    !(last.type==ParserItemTypes::MARKER &&
 		      ((ParserItem_marker&)last).marker==Markers::START)) {
 			ParserItem_marker* newEnd;
-			newEnd=new ParserItem_marker(eos_marker);
+			newEnd=new ParserItem_marker(end_marker);
 			parserItems.push_back(newEnd);
-			sos=true;
+			start_of_sentence=true;
 		}
 	}
 
 	// Mend end of stack if needed.
-        ParserItem& lastParItm=*parserItems.back();
-	if (lastParItm.type==ParserItemTypes::MARKER &&
-	    ((ParserItem_marker&)lastParItm).marker==Markers::START) {
+        ParserItem& last=*parserItems.back();
+	if (last.type==ParserItemTypes::MARKER &&
+	    ((ParserItem_marker&)last).marker==Markers::START) {
 		parserItems.pop_back();
-	} else if (lastParItm.type==ParserItemTypes::WORD) {
+	} else if (last.type==ParserItemTypes::WORD) {
 		ParserItem_marker* newEnd;
 		newEnd=new ParserItem_marker(Markers::PERIOD);
 		parserItems.push_back(newEnd);
@@ -124,21 +124,20 @@ void Quoter::feed_stream(std::istream& in) {
 	lastCol=(unsigned int)Markers::START;
 	// First parser item has been accounted for,
 	// so start iterator on second element.
-	for (std::vector<ParserItem*>::iterator pItm=parserItems.begin()+1;
-	     pItm!=parserItems.end(); ++pItm) {
-		switch ((*pItm)->type) {
+	for (std::vector<ParserItem*>::iterator token=parserItems.begin()+1;
+	     token!=parserItems.end(); ++token) {
+		switch ((*token)->type) {
 		case ParserItemTypes::MARKER:
 			{
 				ParserItem_marker& mark_pi=
-					*((ParserItem_marker*)(*pItm));
+					*((ParserItem_marker*)(*token));
 				row=(unsigned int)mark_pi.marker;
 				break;
 			}
 		case ParserItemTypes::WORD:
 			{
 				ParserItem_word& word_pi=
-					*((ParserItem_word*)(*pItm));
-
+					*((ParserItem_word*)(*token));
 				// Find row index of word in bigram array.
 				row=0;
 				std::vector<std::string>::iterator w;
@@ -179,9 +178,9 @@ void Quoter::feed_stream(std::istream& in) {
 	}
 
 	//Delete parser items.
-	for (std::vector<ParserItem*>::iterator pItm=parserItems.begin();
-	     pItm!=parserItems.end(); ++pItm)
-		delete *pItm;
+	for (std::vector<ParserItem*>::iterator token=parserItems.begin();
+	     token!=parserItems.end(); ++token)
+		delete *token;
 }
 
 void Quoter::feed_file(std::string filePath) {
